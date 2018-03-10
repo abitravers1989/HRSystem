2d88b48503a0b55e36cfc23b078e419be33fd9ea
AbiTravers <abigail.travers@bglgroup.com>
2018-03-10 10:26:51 +0000

    Adding constructor to manager with takes paramaters for testing

--- Manager.cs
+++ Manager.cs
@@ -11,5 +11,13 @@
             this.Hi = true;
         }
 
+        public Manager(int cpc, int ppc) : this()
+        {
+            base.Bsa = 50000;
+            base.Bon = 10;
+            this.Cpc = cpc;
+            this.Ppc = ppc;
+            this.Hi = true;
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -8,15 +8,19 @@ namespace HRSystem
         static void Main(string[] args)
         {
             Manager manager = new Manager();
-            Console.WriteLine(manager.GetTotalPension());
-            var jArray = new JArray("sdfddfg", 90, "gg");
-            Console.WriteLine(jArray[1]);
-            var jobject = new JObject(new JProperty("field_source_string", "ggg"), new JProperty(@"[{""field_target_string"": 1,""value"": 2},{""field_target_string"": 4,""value"": 56}]")
-            );
-            Console.WriteLine(jobject["field_source_string"]);
-            //Console.WriteLine(JObject.Parse("dsfdsff"));
+            Console.WriteLine("Preset Values:" + manager.GetTotalPension());
 
-        Console.ReadKey();
+            Manager managerTwo = new Manager(999, 5);
+            Console.WriteLine(managerTwo.GetTotalPension());
+
+            Console.ReadKey();
         }
     }
 }
+
+//var jArray = new JArray("sdfddfg", 90, "gg");
+//Console.WriteLine(jArray[1]);
+//var jobject = new JObject(new JProperty("field_source_string", "ggg"), new JProperty(@"[{""field_target_string"": 1,""value"": 2},{""field_target_string"": 4,""value"": 56}]")
+//);
+//Console.WriteLine(jobject["field_source_string"]);
+//Console.WriteLine(JObject.Parse("dsfdsff"));
